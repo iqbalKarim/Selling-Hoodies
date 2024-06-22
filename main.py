REFRACTORING and fixing autocast issue with gradient clipping

main.py
@@ -1,14 +1,5 @@
-import torch
-
-from load_data import load_data
-from trainer import train_WGAN
-from trainer2 import train_wgan
-from classes import Discriminator, Generator, Discriminator256, Generator256
-from torchvision import transforms
-from torchvision.utils import save_image, make_grid
-import matplotlib.pyplot as plt
-from utilities import generate_samples
-
+# from trainer2 import train_wgan
+from WGAN.tester import W_GAN_MNIST
 
 if __name__ == '__main__':
     # model_path = "./models/"
@@ -18,20 +9,25 @@ if __name__ == '__main__':
 
     # train_WGAN(dataloader)
 
-    batch_size = 64
-    dataloader = load_data(download=False, batch_size=batch_size, MNIST=False, show_samples=False, size=(256, 256))
-
-    D = Discriminator256()
-    G = Generator256()
-    train_wgan(D, G, dataloader, 64, batch_size=batch_size, device='cuda')
-
-    G.to('cpu')
-    D.to('cpu')
-    G, D = G.eval(), D.eval()
-
-    fake_samples = generate_samples(G, 'final')
-
-    # save  models
-    torch.jit.save(torch.jit.trace(G, torch.rand(batch_size, 64, 1, 1)), 'results/G_model256.pth')
-    torch.jit.save(torch.jit.trace(D, fake_samples), 'results/D_model256.pth')
+    W_GAN_MNIST()
+    
+    # batch_size = 64
+    # dataloader = load_data(download=False, batch_size=batch_size, MNIST=False,
+    #                        show_samples=False, num_samples=10, size=(256, 256))
+    #
+    # D = Discriminator256()
+    # G = Generator256()
+    # y_G, y_D = train_WGAN(G, D, dataloader, latent_dim=64, batch_size=batch_size, device='cuda', g_lr=0.01, d_lr=0.01, n_epochs=1)
+    #
+    # save_graph(y_G, y_D)
+    #
+    # G.to('cpu')
+    # D.to('cpu')
+    # G, D = G.eval(), D.eval()
+    #
+    # fake_samples = generate_samples(G, 'final')
+    #
+    # # save  models
+    # torch.jit.save(torch.jit.trace(G, torch.rand(batch_size, 64, 1, 1)), 'results/G_model256.pth')
+    # torch.jit.save(torch.jit.trace(D, fake_samples), 'results/D_model256.pth')
 

utilities.py
@@ -2,8 +2,6 @@ import torch
 import matplotlib.pyplot as plt
 from torchvision.utils import save_image, make_grid
 
-# JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
-#             119832  gpgpuALL interact    ik323  R       1:22      1 cloud-vm-47-218
 
 def generate_samples(generator, output_name, show_seperate_plots = False, num_samples=10, latent_d=64):
     with torch.no_grad():
@@ -23,3 +21,27 @@ def generate_samples(generator, output_name, show_seperate_plots = False, num_sa
                 plt.show()
 
         return samples
+
+
+def save_graph(y_D, y_G, filepath="./results"):
+    # x = np.linspace(0, 10, 100)
+    fig, ax = plt.subplots()
+    
+    ax.plot(y_D, linewidth=2.0, color='green', label='Discriminator Loss')
+    ax.plot(y_G, linewidth=2.0, color='red', label='Generator Loss')
+    ax.legend()
+    # ax.set(xlim=(0, 50))
+    ax.axis('auto')
+    # plt.show()
+    plt.savefig(filepath+"/training_graph.png")
+    plt.close()
+
+
+def generate_fake_images_from_generator(generator, latent_dim, batch_size,
+                                        file_path='./results/output.jpg', device='cpu'):
+    with torch.no_grad():
+        noise = torch.rand(batch_size, latent_dim, 1, 1, device=device)
+        generated = generator(noise)
+        generated = make_grid(generated, nrow=8)
+        generated = generated.cpu()
+        save_image(generated, file_path)