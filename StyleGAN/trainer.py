3671b1ea5f6dd6e4a98e49467cdd528f7db8f3a5
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2024-07-04 14:35:22 +0100

    normalize input

--- StyleGAN/trainer.py
+++ StyleGAN/trainer.py
@@ -34,6 +34,10 @@ def get_loader(image_size=256, device='cpu'):
             transforms.Resize((image_size, image_size)),
             transforms.ToTensor(),
             transforms.RandomHorizontalFlip(p=0.5),
+            transforms.Normalize(
+                [0.5 for _ in range(CHANNELS_IMG)],
+                [0.5 for _ in range(CHANNELS_IMG)],
+            )
         ]
     )
     batch_size = BATCH_SIZES[int(log2(image_size / 4))]
